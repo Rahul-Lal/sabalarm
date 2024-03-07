86c43014be95d313d62d248dbc6139d5f0a8e926
Rahul Lal <Rahul21Lal@gmail.com>
2024-03-08 11:59:00 +1300

    Long time no see!

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-alert('SLEEP MOTHER-F*CKA!!!');
+/*
+    functions: myFunction(), onYouTubeIframeAPIReady(), onPlayerReady(event), onPlayerStateChange(event), stopVideo() credited to link below
+    - Link: https://stackoverflow.com/questions/42711959/play-youtube-video-on-click
+*/
+
+function alarmset() {
+    let wakeuptime = document.getElementById("date-time").value;
+    console.log("The set time is " + wakeuptime);
+
+    const now = new Date();
+    const currentDateTime = now.toLocaleString();
+    console.log("The actual time is " + currentDateTime);
+
+    if (wakeuptime == currentDateTime) {
+        onPlayerReady()
+    }
+}
