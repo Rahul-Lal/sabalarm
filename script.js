55669259ddaafa27244aa6a92d524c92d33289ff
Rahul Lal <Rahul21Lal@gmail.com>
2024-03-12 16:08:24 +1300

    Update plus audio file

--- script.js
+++ script.js
@@ -2,16 +2,33 @@
     functions: myFunction(), onYouTubeIframeAPIReady(), onPlayerReady(event), onPlayerStateChange(event), stopVideo() credited to link below
     - Link: https://stackoverflow.com/questions/42711959/play-youtube-video-on-click
 */
+// location.reload();
+let ghostdivison = new Audio("sabaton-ghost-division-made-with-Voicemod.mp3");
+let realtime = document.getElementById("h2time");
+setInterval(livetime, 5000);
+
+// create Date object for current location
+function livetime() {
+    let h2time = document.getElementById("h2time").value;
+    let date = new Date().toLocaleString("en-NZ");
+    let time = new Date().toLocaleTimeString("en-NZ");
+    // console.log(date);
+    h2time = time;
+    realtime.innerHTML = realtime.innerHTML.replace(h2time);
+    console.log(realtime);
+}
 
 function alarmset() {
     let wakeuptime = document.getElementById("date-time").value;
     console.log("The set time is " + wakeuptime);
 
-    const now = new Date();
+    const now = new Date().toLocaleString("en-NZ");
+    let polisheddate  = date.replace(", ", " : ");
     const currentDateTime = now.toLocaleString();
     console.log("The actual time is " + currentDateTime);
 
     if (wakeuptime == currentDateTime) {
-        onPlayerReady()
+        alert("Wake Up!");
+        ghostdivison.play();
     }
 }
