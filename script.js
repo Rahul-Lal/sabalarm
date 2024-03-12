01b8b5818447259957a5263717c8b60ce12a69f8
Rahul Lal <Rahul21Lal@gmail.com>
2024-03-12 21:41:37 +1300

    Live Clock works
    Declare script tag in html before the closing body tag,

--- script.js
+++ script.js
@@ -4,26 +4,25 @@
 */
 // location.reload();
 let ghostdivison = new Audio("sabaton-ghost-division-made-with-Voicemod.mp3");
+let time = new Date().toLocaleTimeString("en-NZ");
+let date = new Date().toLocaleString("en-NZ");
 let realtime = document.getElementById("h2time");
-setInterval(livetime, 5000);
+setInterval(livetime, 1000);
 
 // create Date object for current location
 function livetime() {
     let h2time = document.getElementById("h2time").value;
-    let date = new Date().toLocaleString("en-NZ");
-    let time = new Date().toLocaleTimeString("en-NZ");
-    // console.log(date);
-    h2time = time;
-    realtime.innerHTML = realtime.innerHTML.replace(h2time);
-    console.log(realtime);
+    let usedtime = new Date().toLocaleTimeString("en-NZ");
+    h2time = usedtime;
+    realtime.innerHTML = h2time;
+    // console.log(h2time);
 }
 
 function alarmset() {
     let wakeuptime = document.getElementById("date-time").value;
     console.log("The set time is " + wakeuptime);
-
     const now = new Date().toLocaleString("en-NZ");
-    let polisheddate  = date.replace(", ", " : ");
+    let polisheddate = date.replace(", ", " : ");
     const currentDateTime = now.toLocaleString();
     console.log("The actual time is " + currentDateTime);
 
